b66bdf54151501621aad808d1cdfe9938130e8b1
Sagil-Kureshi <sagilkureshi007@gmail.com>
2025-08-23 22:39:57 +0530

    Add files via upload

---
+++ even_pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int n;
+    clrscr();
+    cout << "Enter Number Of Rows : ";
+    cin >> n;
+    for(int s=1;s<=n;s++){
+        for(int k=1;k<s;k++){
+            if(k%2==0)
+                cout << k<<" ";
+            else
+                cout <<"# ";
+        }
+        cout <<endl;
+    }
+    getch();
+}
